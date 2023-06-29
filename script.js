cabf6af8948bb3f168bdfc8a69af38adcbe5db62
Theo0259 <theodewulf.pro@gmail.com>
2023-06-29 16:25:01 +0200

    commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Votre code Javascript
+// document ready en js
+
+const divs = document.querySelectorAll('div.bw');
+
+    // Vérifiez s'il y a au moins un élément <div>
+    if (divs.length > 0) {
+      // Affichez l'image en modifiant la propriété "display" du premier élément <div>
+    divs[0].style.display = 'block';
+    }
+
+    const desc = document.querySelectorAll('div.desc');
+
+    // Vérifiez s'il y a au moins un élément <div>
+    if (desc.length > 0) {
+      // Affichez l'image en modifiant la propriété "display" du premier élément <div>
+    desc[0].style.display = 'block';
+    }
